c0fc45e92d1c51a58ec74f13329629d75d4f7862
Liron Tz <LironTz@Lirons-MacBook-Pro.local>
2012-02-21 21:28:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zimmerman::Application.config.secret_token = '92c472d78980670de7674ca2f5221b062b67d3a5d6e50eca2850599ae602254a469313df6719e45141b2373fa7c262644bf10d27ead10596bd411e43b0da9685'
