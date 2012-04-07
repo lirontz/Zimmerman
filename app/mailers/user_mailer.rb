cd9dd33d0ab8adb9201b771ff4a9dbfbb64c8dc8
lirontz <lirontz@gmail.com>
2012-04-07 17:19:32 +0300

    request mail - hebrew fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,19 +1,20 @@
+# -*- coding: utf-8 -*-
 class UserMailer < ActionMailer::Base
   default :from => "customerservices@missz.com"
 
   def registration_confirmation(user)
   	@user = user
-  	mail(:to => user.email, :subject => "Miss Zimmer - Registration confirmation.")
+  	mail(:to => user.email, :subject => "גברת צימר - אישור הרשמה.")
   end
 
   def request_confirmation(user, request)
   	@user = user
   	@request = request
-  	mail(:to => user.email, :subject => "Miss Zimmer - Request confirmation.")
+  	mail(:to => user.email, :subject => "גברת צימר - אישור בקשה.")
   end
 
   def failure(user)
   	@user = user
-  	mail(:to => user.email, :subject => "Miss Zimmer - Error.")
+  	mail(:to => user.email, :subject => "גברת צימר - שגיאה בתהליך.")
   end
 end
