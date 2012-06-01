23ea24dd6ed404c58d72669b2323cca178bc66e6
lirontz <lirontz@gmail.com>
2012-06-01 23:02:19 +0300

    user editor - change password added.

--- app/controllers/user/cp_controller.rb
+++ app/controllers/user/cp_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class User::CpController < ApplicationController
 	before_filter :authenticate_user!
 
@@ -11,7 +12,15 @@ class User::CpController < ApplicationController
 		@user_details_focused= "active"
 		@user_details_focused_active = "in"
 		@requests = Request.where(:user_id => current_user.id)
-		current_user.update_attributes(params[:user])
+		
+		#current_user.update_attributes(params[:user])
+		
+		if current_user.update_with_password(params[:user])
+		  sign_in(current_user, :bypass => true)
+		  flash[:notice] = 'פרטיך עודכנו בהצלחה!'
+		else
+		  flash[:notice] = 'העדכון נכשל!'
+		end
 		render :index
 	end
 end
